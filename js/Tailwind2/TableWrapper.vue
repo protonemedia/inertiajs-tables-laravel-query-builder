4c2c9305024fe395c0ff2099a3bb90be88ff061a
pascalbaljet <pascal@pascalbaljet.nl>
2021-04-03 21:55:40 +0200

    Update TableWrapper.vue

--- js/Tailwind2/TableWrapper.vue
+++ js/Tailwind2/TableWrapper.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex flex-col mt-2">
-    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+    <div class="-my-2 sm:-mx-6 lg:-mx-8">
       <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg relative">
+        <div class="shadow border-b border-gray-200 relative">
           <slot />
         </div>
       </div>
