6e2699d27a6cdf49ca8741ce9c19251f8a3dab76
pascalbaljet <pascal@pascalbaljet.nl>
2022-08-03 00:02:54 +0200

    Move to packed version

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-const path = require("path")
-const { defineConfig } = require("vite")
-import vue from "@vitejs/plugin-vue"
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
+
     build: {
         lib: {
-            entry: path.resolve(__dirname, "js/main.js"),
+            entry: resolve(__dirname, "js/main.js"),
             name: "Inertia.js Tables for Laravel Query Builder",
             fileName: (format) => `inertiajs-tables-laravel-query-builder.${format}.js`
         },
@@ -21,8 +22,7 @@ export default defineConfig({
             ],
             output: {
                 globals: {
-                    vue: "Vue",
-                    Vue: "Vue"
+                    vue: "Vue"
                 }
             }
         }
