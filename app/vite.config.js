12d927bc34ca0f8d94687666a48342b48f5580f6
pascalbaljet <pascal@pascalbaljet.nl>
2022-08-03 00:09:14 +0200

    Update vite.config.js

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,21 +1,20 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
-const path = require("path")
 
 export default defineConfig({
-    resolve:{
-        alias: {
-            "vue": path.resolve("./node_modules/vue")
-        }
-    },
     plugins: [
         laravel([
             "resources/css/app.css",
             "resources/js/app.js",
         ]),
         vue({
-
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
         }),
     ]
 });
